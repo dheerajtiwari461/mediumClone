751a858e646df0c5c2dcfdb0040d0f86143faeec
Dheeraj Tiwari <dheerajtiwari461@outlook.com>
2024-06-22 08:51:38 +0530

    feat: Add Google sign-in option and link to signup page

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -1,4 +1,6 @@
 import InputBox from "../components/input.component";
+import googleIcon from "../imgs/google.png";
+import {Link} from "react-router-dom";
 
 const UserAuthForm = ({ type }) => {
   return (
@@ -39,8 +41,31 @@ const UserAuthForm = ({ type }) => {
         </button>
 
         <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
-
+          <hr className="w-1/2 border-black" />
+          <p>or </p>
+          <hr className="w-1/2 border-black" />
         </div>
+
+        <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
+          <img src={googleIcon} className="w-5"/>
+          Continue with Google
+        </button>
+
+        {
+
+          type == "sign-in" ?
+           <p className="mt-6 text-dark-grey text-xl text-center">Don't have an account ?
+          <Link to="/signup" className="underline text-black text-xl ml-1">
+            Join us today
+           </Link>
+           </p>
+          :<p className="mt-6 text-dark-grey text-xl text-center">Already a member ?
+          <Link to="/signin" className="underline text-black text-xl ml-1">
+          Sign in here
+           </Link>
+           </p>
+
+        }
 
       </form>
     </section>
