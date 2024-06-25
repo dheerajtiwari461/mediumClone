1901f9fe7d273bf7bea00aae5907db892963a226
Dheeraj Tiwari <dheerajtiwari461@outlook.com>
2024-06-25 22:03:05 +0530

    chore: Update signup API endpoint and request body

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
+import bcrypt from "bcrypt";
+import User from "./Schema/User.js";
 
 const server = express();
 let PORT = /*process.env.PORT ||*/ 3000;
+
+let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
 
 server.use(express.json());
 
@@ -12,7 +17,41 @@ mongoose.connect(process.env.DB_LOCATION,{
 })
 
 server.post("/signup", (req,res)=>{
+    let {fullname, email, password} = req.body; 
+
+    // validating the data from frontend
+    if(fullname.length < 3){
+        return res.status(403).json({"error": "fullname must be at least 3 letters long"});
+    }
+    if (!email.length) {
+        return res.status(403).json({"error": "Enter a valid email"});
+    }
+    if(!emailRegex.test(email)){
+        return res.status(403).json({"error": "Email is invalid"});
+
+    }
+    if (!passwordRegex.test(password)) {
+        return res.status(403).json({"error": "Password should be 6 to 20 characters long and should contain atleast one number, one uppercase and one lowercase character"});
+    }
     
+    // hashing the password
+    bcrypt.hash(password, 10, (err, hashed_password)=>{
+        let username = email.split("@")[0];
+        
+        let user = new User({
+            personal_info: {
+                fullname,
+                email,
+                password: hashed_password,
+                username
+            }
+        })
+
+        console.log(hashed_password);
+        
+    })
+    return res.status(200).json({"status": "okay"});
+
 })
 
 
