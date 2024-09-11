77f2d0e9e007ccda95ba8ec946cc3cd8f395f3d2
Dheeraj Tiwari <dheerajtiwari461@outlook.com>
2024-09-11 12:02:39 +0530

    Store user in sessions

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -2,20 +2,25 @@
 import AnimationWrapper from "../common/page-animation";
 import InputBox from "../components/input.component";
 import googleIcon from "../imgs/google.png";
-import {Link} from "react-router-dom";
+import {Link, Navigate} from "react-router-dom";
 import {Toaster, toast} from "react-hot-toast";
 import axios from "axios";
 import { storeInSession } from "../common/session";
+import { useContext, useRef } from "react";
+import { UserContext } from "../App";
 
 const UserAuthForm = ({ type }) => {
 
+  const authForm = useRef();
+  let{userAuth: {access_token}, setUserAuth } = useContext(UserContext);
+  
  
  const userAuthThroughService=(serverRoute, formData)=>{
 
   axios.post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, formData)
   .then(({data})=>{
     storeInSession("user", JSON.stringify(data));
-    console.log(sessionStorage);
+    setUserAuth(data);
     
   })
   .catch(({response})=>{
@@ -67,6 +72,9 @@ userAuthThroughService(serverRoute, formData);
 }
 
   return (
+    access_token ?
+    <Navigate to="/"/>
+    :
       <AnimationWrapper keyValue={type}>
     <section className="h-cover flex items-center justify-center">
     <Toaster/>
